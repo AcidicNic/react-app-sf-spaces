431265008f48912a920b54d17f21992991d6ee8a
Nicole Abadie <nic4096@gmail.com>
2021-11-21 18:52:18 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import './Title.css';
+
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Pumpkin Finder</h1>
+      <div className="Title-Subtitle"> (The best pumpkin patches in the Bay Area!)</div>
+    </div>
+  )
+}
+
+export default Title
